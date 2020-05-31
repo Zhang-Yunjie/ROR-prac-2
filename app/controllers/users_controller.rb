25cb60d62cd803a9babde2ecb71fbfbe8c871b0e
Zhang Yunjie <e0421414@u.nus.edu>
2020-05-31 22:20:22 +0800

    User sign up and edition

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,38 @@
+class UsersController < ApplicationController
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save 
+          flash[:notice] = "Welcome to NUSchedule, #{@user.username}!" 
+          redirect_to root_path
+
+        else 
+            render 'new'
+        end
+    end
+
+    def edit 
+        @user = User.find(params[:id])
+    end
+
+    def update
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+            flash[:success] = "Your account was updated successfully"
+            redirect_to root_path
+        else
+            render 'edit'
+        end
+    end
+
+
+    private
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
+    end
+
+
+end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   get 'info/about'
   get 'welcome/home'
+  get 'signup', to: 'users#new'
+  post 'users', to: 'users#create'
+  
+  resources :users
+
   root 'welcome#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
