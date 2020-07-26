03fa351625431a8b2ecc855d9d8b16a014f1bad3
aiyuhang <aiyuhang@wityun.com>
2020-07-26 12:13:32 +0800

    change

--- app/models/user_mod.rb
+++ app/models/user_mod.rb
@@ -2,12 +2,20 @@ class UserMod < ApplicationRecord
   belongs_to :user
   belongs_to :mod
 
-  validates prerequisite
+  # validates prerequisite
+  validate :prerequisite_class
 
-  def prerequisite
-    @mod.find{ |m| puts m.title == "cs1010s" }
-    if puts mod.module_title == "cs2040" && UserMod.find_by(mods: @mod)
-      errors.add('Need to take CS1101s first before taking CS2040')
+  # def prerequisite
+  #   @mod.find{ |m| puts m.title == "cs1010s" }
+  #   if puts mod.module_title == "cs2040" && UserMod.find_by(mods: @mod)
+  #     errors.add('Need to take CS1101s first before taking CS2040')
+  #   end
+  # end
+
+  def prerequisite_class
+    text = self.mod&.module_prerequisite
+    if text.present?
+      errors.add("mod_id", "#{text} must read before reading module_title!")
     end
   end
 end
