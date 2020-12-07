39bbd47923ed28cef93494cc93499965d25877e9
Zhang-Yunjie <61343221+Zhang-Yunjie@users.noreply.github.com>
2020-12-07 20:45:58 +0800

    Update user_mod.rb
    Clear codes

--- app/models/user_mod.rb
+++ app/models/user_mod.rb
@@ -17,26 +17,18 @@ class UserMod < ApplicationRecord
   validate :prerequisite_class
   validate :check_uniq
 
-  # def prerequisite
-  #   @mod.find{ |m| puts m.title == "cs1010s" }
-  #   if puts mod.module_title == "cs2040" && UserMod.find_by(mods: @mod)
-  #     errors.add('Need to take CS1101s first before taking CS2040')
-  #   end
-  # end
-
   def prerequisite_class
     text = self.mod&.module_prerequisite
     mod_id = Mod.find_by(module_code: text) 
     if UserMod.find_by(user_id: self.user_id, mod_id: mod_id).blank?
-      errors.add("mod_id", "#{text} must read before reading this module")
+      errors.add("mod_id", "#{text} must be read before reading this module")
     end
   end
 
 
-
   def check_uniq
     if UserMod.find_by(user_id: self.user_id, mod_id: self.mod_id, semester_id: self.semester_id)
-      errors.add("mod_id", "You already added this mod!")
+      errors.add("mod_id", "You have already added this mod!")
     end
   end
 end
